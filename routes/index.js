a7b9dd7599b9836e9ba5ab0e405ba81a0569a133
pedroll <pedrollongo@gmail.com>
2020-02-12 22:36:03 +0100

    primera ruta y nodemon

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// next utilizado para middleware
+router.get('/', (req, res, next) => {
+    res.status(200).json(
+        {
+            ok: true,
+            mensaje: 'peticion realizada correctamente'
+        }
+    );
 });
 
 module.exports = router;
