1feacb9589b09840e1be0d3e59d45bb37c3ea998
pedroll <pedrollongo@gmail.com>
2020-02-13 10:06:35 +0100

    mongoose conexion base de datos

--- app.js
+++ app.js
@@ -1,21 +1,52 @@
-var express = require('express');
-//var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let express = require('express');
+//let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
 
-var app = express();
+let app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// conexion BD
+// mongoose.conexion.openUri('mongodb://localhost:27017/hospitaldb',{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+mongoose.connect('mongodb://localhost:27017/hospitaldb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('conectado a base de datos');
+    // we're connected!
+});
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb://localhost:27017/hospitaldb?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+//     client.close();
+// });
 module.exports = app;
+//module.exports = mongoose;
 
